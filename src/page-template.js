e0be2f8fbf8da1797527605f5714d145f29b44af
Stim Fry Mac <timothy.atoyo@gmail.com>
2024-01-30 18:17:22 +0000

    feature: adds collumns to each card and css file with styles

--- src/page-template.js
+++ src/page-template.js
@@ -4,57 +4,63 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        <div class="mb-3 col-12 col-md-3 col-lg-4 d-flex align-items-stretch justify-content-stretch ">
+            <div class="card employee-card  align-items-stretch justify-content-stretch ">
+            <div class="card-header">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item border-0 bg-transparent">ID: ${manager.getId()}</li>
+                    <li class="list-group-item border-0 bg-transparent">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item border-0 bg-transparent">Office number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+                </div>
         </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item border-0 bg-transparent">ID: ${manager.getId()}</li>
-                <li class="list-group-item border-0 bg-transparent">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item border-0 bg-transparent">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
         `;
     };
 
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item border-0 bg-transparent">ID: ${engineer.getId()}</li>
-            <li class="list-group-item border-0 bg-transparent">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item border-0 bg-transparent">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
+        <div class="mb-3 col-12 col-md-3 col-lg-4 d-flex align-items-stretch justify-content-stretch ">
+            <div class="card employee-card align-items-stretch justify-content-stretch ">
+                <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+                </div>
+                <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item border-0 bg-transparent">ID: ${engineer.getId()}</li>
+                <li class="list-group-item border-0 bg-transparent">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item border-0 bg-transparent">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+            </ul>
+                </div>
+            </div>
+        </div>
         `;
     };
 
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item border-0 bg-transparent">ID: ${intern.getId()}</li>
-            <li class="list-group-item border-0 bg-transparent">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item border-0 bg-transparent">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
+        <div class="mb-3 col-12 col-md-3 col-lg-4  d-flex align-items-stretch justify-content-stretch ">
+            <div class="card employee-card align-items-stretch justify-content-stretch ">
+                <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+                </div>
+                <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item border-0 bg-transparent">ID: ${intern.getId()}</li>
+                <li class="list-group-item border-0 bg-transparent">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item border-0 bg-transparent">School: ${intern.getSchool()}</li>
+            </ul>
+                </div>
+            </div>
+        </div>
         `;
     };
 
@@ -93,7 +99,7 @@ module.exports = team => {
     <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./styles.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
@@ -107,9 +113,9 @@ module.exports = team => {
     </div>
     <div class="container">
         <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            
                 ${generateTeam(team)}
-            </div>
+            
         </div>
     </div>
 </body>
